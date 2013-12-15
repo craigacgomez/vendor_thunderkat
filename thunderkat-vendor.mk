ffe56d9843f83986ad1c79d9c6e35e7835717912
Craig Gomez <craigacgomez@gmail.com>
2013-12-14 18:05:07 -0800

    Add Google setup properties

--- thunderkat-vendor.mk
+++ thunderkat-vendor.mk
@@ -12,3 +12,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=craigacgomez \
     ro.goo.rom=ThunderKat \
     ro.goo.version=$(shell date +%Y%m%d)
+
+# Setup properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.android.dataroaming=false \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.wifi_required=true \
+    keyguard.no_require_sim=true \
+    ro.build.selinux=1
